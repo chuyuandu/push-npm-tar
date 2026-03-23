c15f21bb1c662b46a01514d64cdce90116dcf524
chuyuan.du <chuyuandu@163.com>
2026-03-23 11:00:25 +0800

    update test files

---
+++ test/args.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { handleArgs } from '../src/index';
+import pkg from '../package.json';
+import { helpContent } from '../src/util';
+
+describe('show version', () => {
+  it('显示版本号', () => {
+    expect(
+      handleArgs({
+        _: [],
+        '--version': true,
+        cwd: '_virtual_',
+      }),
+    ).toEqual(pkg.version);
+  });
+});
+
+describe('show help', () => {
+  it('显示帮助信息', () => {
+    expect(
+      handleArgs({
+        _: [],
+        '--help': true,
+        cwd: '_virtual_',
+      }),
+    ).toEqual(helpContent);
+  });
+});
